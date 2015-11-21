9cfe9768be5e6a625bd7c760eeaa639108885c55
Naila Nur <nnur@Dylans-MacBook-Pro.local>
2015-11-21 14:06:10 -0800

    added gulp and sass

--- public/common/sideBar/list-pane/list-pane-directive.js
+++ public/common/sideBar/list-pane/list-pane-directive.js
@@ -1,5 +1,9 @@
 function listPane() {
     return {
+        scope: {
+            listItems: '=data'
+        },
+        restrict: 'E',
         templateUrl: 'common/sidebar/list-pane/list-pane.html'
     };
 }
--- public/components/main/main-controller.js
+++ public/components/main/main-controller.js
@@ -3,5 +3,14 @@
  * @param {Object} jsonReader - gets the contact data from the json file
  */
 function MainController(jsonReader, baseURL) {
-    jsonReader.getJsonData(baseURL + '/assets/contacts.json');
+    // required for use with promises
+    var self = this;
+
+    // evaluating promise
+    jsonReader.getJsonData(baseURL + '/assets/contacts.json')
+        .then(function(res) {
+            self.contactList = res.data.contacts;
+        });
+
+
 }
